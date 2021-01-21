ebe1cd1553f1b51d5c18c76c6f597b047e383023
Josiney Junior <josineyjunior14@gmail.com>
2021-01-21 12:22:46 -0300

    inferencia de tipos

---
+++ secao-4/inferencia-tipos/app.ts
@@ -0,0 +1,5 @@
+let menssagem: string = "Hello World";
+let temporadasFriends: number = 3;
+let estudandoAngular: boolean = true;
+let listaDeFrutas: Array<string> = ["maçã", "banana", "pera"];
+let notasDasProvas: Array<number> = [12, 13, 14, 11];
